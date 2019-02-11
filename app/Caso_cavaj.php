863502b8d64957f8580f039dcbf7650e8b2b09a3
lucky snchz <xul27@hotmail.com>
2019-02-11 12:49:19 -0300

    agrego caso

---
+++ app/Caso_cavaj.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Caso_cavaj extends Model
+{
+  public $table="caso_cavaj";
+  public $timestamps=false;
+  public $guarded=[];
+}
